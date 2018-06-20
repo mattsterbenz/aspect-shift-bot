63891068626523a064888946b1d37e909d1bf255
mattsterbenz <matt@sterbe.nz>
2018-06-19 21:20:15 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on("guildMemberAdd", (member) => {
+  member.addRole("458417658014990358").catch(console.error);
+  member.addRole("458423243812896768").catch(console.error);
+});
+
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
   }
 });
 
-client.login('token');
+client.login('NDU4NzQ3MjMxMTE0MTAwNzQ2.Dgss5Q.qkgZBfFRvkVtbz5B_H0yvoVDi-k');
